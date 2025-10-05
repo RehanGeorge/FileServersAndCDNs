dcaa5d6c6ffd0e9085877a1d8c277c6afcc02641
RehanGeorge <rehan.george@gmail.com>
2025-10-05 18:36:40 +0530

    Update thumbnail handler to store thumbnails on the file server

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -33,35 +34,36 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// Implement upload logic
-	const maxMemory = 10 << 20 // 10 MB using bit-shift
-	err = r.ParseMultipartForm(maxMemory)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Couldn't parse form", err)
-		return
-	}
-
-	// Get the image data from the form
-	file, _, err := r.FormFile("thumbnail")
+	file, fileHeader, err := r.FormFile("thumbnail")
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Couldn't get file from form", err)
 		return
 	}
 	defer file.Close()
 
-	mediaType := r.Header.Get("Content-Type")
+	mediaType := fileHeader.Header.Get("Content-Type")
 
-	// Read all image data into a byte slice
-	data, err := io.ReadAll(file)
+	// Create a unique file path
+	pathToFile := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%s", videoID.String(), mimeToExt(mediaType)))
+
+	// Create the file
+	newFile, err := os.Create(pathToFile)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't read file", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create file", err)
+		return
+	}
+	defer newFile.Close()
+
+	// Copy the contents from the multipart.File to the new file on disk using io.Copy
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't save file", err)
 		return
 	}
 
-	// Encode the image data to base64
-	base64Data := base64.StdEncoding.EncodeToString(data)
-	dataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, base64Data)
+	// Update the thumbnail URL in the database
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, filepath.Base(pathToFile))
 
-	// Get video metadata from DB
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't get video metadata", err)
@@ -73,10 +75,8 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	// Update the video's thumbnail URL
-	video.ThumbnailURL = &dataURL
+	video.ThumbnailURL = &thumbnailURL
 
-	// Save updated video metadata to DB
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't update video metadata", err)
@@ -85,4 +85,17 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	// Respond with updated JSON of the video's metadata
 	respondWithJSON(w, http.StatusOK, video)
+}
+
+func mimeToExt(mime string) string {
+	switch mime {
+	case "image/jpeg":
+		return "jpg"
+	case "image/png":
+		return "png"
+	case "image/gif":
+		return "gif"
+	default:
+		return "bin" // default extension
+	}
 }
